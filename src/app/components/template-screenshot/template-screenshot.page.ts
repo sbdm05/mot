091f15c031587d6f35b58e53c33c3bec55a78964
Sonia-Baibou <s.baibou@ohmycode.io>
2024-02-09 15:46:58 +0100

    fix margin unit

--- src/app/components/template-screenshot/template-screenshot.page.ts
+++ src/app/components/template-screenshot/template-screenshot.page.ts
@@ -56,4 +56,45 @@ export class TemplateScreenshotPage implements OnInit {
         console.log('error', error);
       });
   }
+
+  downloadLetterCanvas() {
+    const img = new Image();
+
+    img.onload = () => {
+      const A4_WIDTH_MM = 210; // Largeur en millimètres
+      const A4_HEIGHT_MM = 297; // Hauteur en millimètres
+      const A4_WIDTH_PX = Math.floor(A4_WIDTH_MM * 2.83); // Convertir en pixels à 72 ppp
+      const A4_HEIGHT_PX = Math.floor(A4_HEIGHT_MM * 2.83); // Convertir en pixels à 72 ppp
+
+      const canvas = document.createElement('canvas');
+      canvas.width = A4_WIDTH_PX;
+      canvas.height = A4_HEIGHT_PX;
+
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, A4_WIDTH_PX, A4_HEIGHT_PX); // Dessiner l'image sur le canvas avec les dimensions A4
+
+      const imageData = canvas.toDataURL('image/jpeg');
+
+      const options = {
+        documentSize: 'A4',
+        type: 'share',
+        // landscape: 'portrait',
+        fileName: 'motiv-pro.pdf',
+      };
+      this.pdfGenerator
+        .fromData(imageData, options)
+        .then((base64) => {
+          console.log('PDF généré avec succès');
+        })
+        .catch((error) => {
+          console.log('Erreur lors de la génération du PDF :', error);
+        });
+    };
+
+    img.onerror = (error) => {
+      console.error('Erreur lors du chargement de image', error);
+    };
+
+    img.src = `data:image/png;base64,${this.base64}`;
+  }
 }
